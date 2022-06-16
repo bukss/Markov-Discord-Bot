b71d109e4a41716a9f7f5958dbb34a7a05ae5c0d
bukss <ephraim408@gmail.com>
2022-06-15 21:51:50 -0400

    lol did the wrong thing

--- Main.js
+++ Main.js
@@ -1,113 +1,45 @@
+const Discord = require("discord.js");
 const MessageHandling = require("./MessageHandling.js");
-const MarkovChain = require("./MarkovChain.js");
-const fs = require("fs");
+const CommandParser = require("./CommandParser");
+const { stringify } = require("querystring");
 
-function correctChannel(channel, client) {
-    commandChannels = MessageHandling.getConfig().commandChannel;
-    if (commandChannels) {
-        return (commandChannels.includes(channel.id));
-    } else {
-        return true;
+const config = MessageHandling.getConfig();
+const oauth = config["token"];
+
+var client = new Discord.Client(config);
+
+client.on('ready', () => {
+    console.log('Connected');
+});
+
+client.on('message', (msg) => {
+    if (msg.author.bot) {
+        console.log("bot detected " + msg.content);
+        return;
     }
-}
 
-module.exports = {
-    checkCommands : async function (msg, client) {
-        const blacklist = MessageHandling.getBlacklist();
-        const config = MessageHandling.getConfig();
-        var text = msg.content;
-        const noCommands = config["noCommands"];
-
-        if (text[0] !== "!") {
-            return;
-        }
-        try {
-            if (config.blacklisted_users.includes(msg.author.id)) {
-                return;
-            }
-        } catch (err) {}
-
-        //If the command is !chain
-        if (text.startsWith(`!chain`) && !noCommands && correctChannel(msg.channel, client)) {
-            //Otherwise, make a chain based on the regular dataset
-            let data = MessageHandling.getData();
-            let markovData = data[0];
-            let chunks = data[1];
-            let startingWords = data[2];
-            MessageHandling.send(MarkovChain.makeChain(markovData, chunks, startingWords,
-                config.minChainLength, config.maxChainLength), msg.channel, client);
-
-        //If the command is blacklist
-        } else if (text.startsWith(`!blacklist`) && config.admins.indexOf(msg.author.id) > -1 && correctChannel(msg.channel, client)) {
-            var command = msg.content.split(" ");
-            //For adding to the blacklist
-            if (command[1] == "add") {
-                if (command[2] == "word") {
-                    let word = command[3];
-                    blacklist.singleWords.push(word);
-                    MessageHandling.send(`Added the word "${word}" to the blacklist`, msg.channel, client);
-
-                } else if (command[2] == "phrase") {
-                    let phrase = command.slice(3).join(" ");
-                    blacklist.fullPhrases.push(phrase);
-                    MessageHandling.send(`Added the phrase "${phrase}" to the blacklist`, msg.channel, client);
-
-                } else if (command [2] == "regex") {
-                    let regex = command[3];
-                    blacklist.regex.push(regex);
-                    MessageHandling.send(`Added the regular expression "${regex}" to the blacklist`, msg.channel, client);
-                }
-            //For removing from the blacklist
-            } else if (command[1] == "remove") {
-                if (command[2] == "word") {
-                    let word = command[3];
-                    let index = blacklist.singleWords.indexOf(command[3]);
-                    if (index > -1) {
-                        blacklist.singleWords.splice(index);
-                        MessageHandling.send(`Removed the word "${word}" from the blacklist`, msg.channel, client);
-                    } else {
-                        MessageHandling.send(`${word} is not a word in the blacklist!`, msg.channel, client);
-                    }
-
-                } else if (command[2] == "phrase") {
-                    let phrase = command.slice(3).join(" ");
-                    let index = blacklist.fullPhrases.indexOf(command[3]);
-                    if (index > -1) {
-                        blacklist.fullPhrases.splice(index);
-                        MessageHandling.send(`Removed the phrase "${phrase}" from the blacklist`, msg.channel, client);
-                    } else {
-                        MessageHandling.send(`${phrase} is not a phrase in the blacklist!`, msg.channel, client);
-                    }
-
-                } else if (command [2] == "regex") {
-                    let regex = command[3];
-                    let index = blacklist.regex.indexOf(command[3]);
-                    if (index > -1) {
-                        blacklist.regex.splice(index);
-                        MessageHandling.send(`Removed the regular expression "${regex}" from the blacklist`, msg.channel, client);
-                    } else {
-                        MessageHandling.send(`${regex} is not a regular expression in the blacklist!`, msg.channel, client);
-                    }
-                }
-                } else {
-                    MessageHandling.send(`That is not the correct syntax for that command. Please refer to the !help command for help`, msg.channel, client);
-                }
-                MessageHandling.clearCache();
-                MessageHandling.setBlacklist(blacklist);
-                fs.writeFile("blacklist.json", JSON.stringify(blacklist), (err) => {
-                    if (err) {
-                        throw err;
-                     }
-                    // success case, the file was saved
-                    MessageHandling.send("Blacklist updated", msg.channel, client);
-                });
-            } else if (text.startsWith(`!clear`) && config.admins.indexOf(msg.author.id) > -1 && correctChannel(msg.channel, client)) {
-                msg.channel.startTyping()
-                MessageHandling.clearCache();
-                MessageHandling.send("Cache cleared", msg.channel, client);
-            } else if (text.startsWith(`!help`) && !noCommands && correctChannel(msg.channel, client)) {
-                var helpText = fs.readFileSync("HelpFile.txt", "utf8");
-                MessageHandling.send(helpText, msg.channel, client);
-        }
+    console.log(msg.content);
+    CommandParser.checkCommands(msg, client);
+    if (msg.content[0] !== "!") {
+        MessageHandling.HandleMessage(msg, client);
     }
-}
+
+});
+
+client.on('error', (err) =>{
+    let me = client.users.cache.get("163041163408965632");
+    let guilds = client.guilds.array();
+    let dm = `Bot that is in: \n \`\`\`\n ${guilds} \n\`\`\` \n received error: \n \`\`\`\n ${err.message} \n\`\`\``;
+    me.send(dm);
+});
+
+client.on('warn', (warning) => {
+    let me = client.users.cache.get("163041163408965632");
+    let guilds = client.guilds.array();
+    let dm = `Bot that is in: \n \`\`\`\n ${guilds} \n\`\`\` \n received warning: \n \`\`\`\n ${warn.message} \n\`\`\``;
+    me.send(dm);
+});
+
+client.login(oauth);
+
+
